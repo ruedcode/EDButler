c49913861085364af35855128da4e65e87c041bf
Eugene Kalyada <Evgeniy.Kalyada@waveaccess.ru>
2017-09-27 11:28:48 +0500

    fixed service

--- EDButler/EDButlerService.swift
+++ EDButler/EDButlerService.swift
@@ -8,63 +8,117 @@
 
 import UIKit
 
-open class EDButlerRequest:NSObject, URLSessionDelegate, URLSessionDataDelegate {
+open class EDButlerRequest:NSObject {
 
 	public var route : EDButlerRoute
-	fileprivate var session: URLSession
+	public var expectedContentLength: Int64 = 0
+	public var downloadedContentLength: Int = 0
+	public var downloadedData: Data?
+	public var didChangedProgress : (()->Void)?
+	public var willPerform : ((URLRequest)->URLRequest)?
 
-	fileprivate init(route: EDButlerRoute, sessionConfig:URLSessionConfiguration) {
+	weak fileprivate var service: EDButlerService?
+
+
+	fileprivate init(route: EDButlerRoute, service:EDButlerService) {
 		self.route = route
-		session = URLSession(configuration: sessionConfig)
-//		self.session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
+		self.service = service
 	}
 
 	public func run<T>(type: T.Type, completion: @escaping ((EDButlerResponse<T>) -> Void)) {
-		sendRequest { (data, response, error) in
+		service?.sendRequest(self, completion: { (data, response, error) in
 			let object = EDButlerResponse<T>(data: data, response: response, error: error)
 			completion(object)
-		}
+		})
 	}
 
 	public func run() {
-		sendRequest { (_, _, _) in}
+		service?.sendRequest(self, completion: { (_, _, _) in})
 	}
-
-	fileprivate func sendRequest(completion: @escaping ((Data?, URLResponse?, Error?)->Void)) {
-		UIApplication.shared.isNetworkActivityIndicatorVisible = true
-		session.dataTask(with: route.request) { (data, response, error) in
-			UIApplication.shared.isNetworkActivityIndicatorVisible = false
-			completion(data, response, error)
-		}
-	}
-
-//
-//	// MARK: - SessionDelegate
-//	public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-//		<#code#>
-//	}
-//
-//	public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-//		<#code#>
-//	}
-
 }
 
-open class EDButlerService {
+open class EDButlerService: NSObject, URLSessionTaskDelegate, URLSessionDelegate, URLSessionDataDelegate {
 
 	open static let `default` = EDButlerService()
-	fileprivate var sessionConfig: URLSessionConfiguration
 
-	public init() {
-		sessionConfig = URLSessionConfiguration.default
+	open static var willPerform : ((URLRequest)->URLRequest)?
+
+	fileprivate var sessionConfiguration: URLSessionConfiguration
+	fileprivate var runnedDataTasks: [URLSessionDataTask:EDButlerRequest] = [:]
+
+	lazy fileprivate var session: URLSession = {
+		let session = URLSession(configuration: self.sessionConfiguration, delegate: self, delegateQueue: nil)
+		return session
+	}()
+
+	public override init() {
+		sessionConfiguration = URLSessionConfiguration.default
 	}
 
 	public init(sessionConfiguration: URLSessionConfiguration) {
-		self.sessionConfig = sessionConfiguration
+		self.sessionConfiguration = sessionConfiguration
 	}
 
 	public func load(_ route: EDButlerRoute)->EDButlerRequest {
-		return EDButlerRequest(route: route, sessionConfig: sessionConfig)
+		return EDButlerRequest(route: route, service: self)
 	}
+
+	public func cancellAll() {
+		for item in runnedDataTasks {
+			item.key.cancel()
+		}
+		runnedDataTasks = [:]
+	}
+
+	public func cancel(_ request: EDButlerRequest) {
+		if let item = runnedDataTasks.filter({ (item) -> Bool in
+			return item.value == request
+		}).first {
+			item.key.cancel()
+		}
+	}
+
+	fileprivate func sendRequest(_ request: EDButlerRequest, completion: @escaping ((Data?, URLResponse?, Error?)->Void)) {
+		//run globall modification
+		var urlRequest = request.route.request
+		if let globalModification = EDButlerService.willPerform {
+			urlRequest = globalModification(urlRequest)
+		}
+		if let localModification = request.willPerform {
+			urlRequest = localModification(urlRequest)
+		}
+		UIApplication.shared.isNetworkActivityIndicatorVisible = true
+		let task = session.dataTask(with: urlRequest) { (data, response, error) in
+			UIApplication.shared.isNetworkActivityIndicatorVisible = false
+			completion(data, response, error)
+		}
+		runnedDataTasks[task] = request
+	}
+
+	// MARK: - URLSession
+	public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+		if let request = runnedDataTasks[dataTask] {
+			request.expectedContentLength = response.expectedContentLength
+			request.downloadedData = Data()
+			request.downloadedContentLength = 0
+			if let action = request.didChangedProgress {
+				action()
+			}
+		}
+	}
+
+	public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+		if let request = runnedDataTasks[dataTask] {
+			request.downloadedData?.append(data)
+			request.downloadedContentLength = request.downloadedData!.count
+		}
+	}
+
+	public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+		if let task = task as? URLSessionDataTask {
+			runnedDataTasks.removeValue(forKey: task)
+		}
+	}
+
 
 }
